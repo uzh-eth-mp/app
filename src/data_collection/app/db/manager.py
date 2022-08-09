226806073aab30836ca5e03a1b59ca522479bb02
Can <can.inan@bluewin.ch>
2022-08-09 14:46:59 +0200

    change block and transaction table, added inserts in db manager

--- src/data_collection/app/db/manager.py
+++ src/data_collection/app/db/manager.py
@@ -37,18 +37,59 @@ class DatabaseManager:
         await self.db.close()
         log.info("Disconnected from PostgreSQL")
 
-    async def insert_block_data(self):
+    async def insert_block_data(self, block_number: int, block_hash: str, nonce: str, difficulty: int,
+                                 gas_limit: int, gas_used: int, timestamp: str, miner: str, parent_hash: str, 
+                                 omner_hash: str, block_reward: float, uncles_reward: float ):
         """
         Insert block data into the database
         """
-        # TODO: Update this method with valid block data given
-        # from the arguments
+
         table = f"block_data_{self.node_name}"
-        hash = "0xb2fb6a01624f285604913697f0f80c8ee86620750be532cc3dc5751cf079662e"
-        difficulty = "12,120,825,516,066,787"
-        gas_limit = "29,970,705"
 
         await self.db.execute(f"""
-            INSERT INTO {table} (hash, difficulty, gas_limit)
-            VALUES ($1, $2, $3);
+            INSERT INTO {table} (block_number, block_hash, nonce, difficulty, gas_limit, gas_used, timestamp, miner, parent_hash, omner_hash, block_reward, uncles_reward)
+            VALUES ($1, $2, $3,$4, $5, $6,$7,$8, $9, $10, $11, $12);
+        """, self.block_number, self.block_hash, self.nonce, self.difficulty, self.gas_limit, self.gas_used, self.timestamp,self.miner,
+             self.parent_hash, self.owner_hash, self.block_reward, self.uncles_reward)
+
+    
+    async def insert_transaction_data(self, transaction_hash: str, block_number: int, from_address: str, to_address: str, value: float, transaction_fee: float,
+                                      gas_price: float, gas_limit: int, gas_used: int, is_token_tx: bool, input_data: str):
+        """
+        Insert transaction data into the database
+        """
+
+        table = f"transaction_data_{self.node_name}"
+
+        await self.db.execute(f"""
+            INSERT INTO {table} (transaction_hash, block_number, from_address, to_address, value, transaction_fee, gas_price, gas_limit, gas_used, is_token_tx, input_data)
+            VALUES ($1, $2, $3,$4, $5, $6,$7,$8, $9, $10, $11);
+        """, self.transaction_hash, self.block_number, self.from_address, self.to_address, self.value, self.transaction_fee, self.gas_price, self.gas_limit, self.gas_used, self.is_token_tx, self.input_data)
+
+
+    async def insert_internal_transaction_data(self):
+        """
+        Insert transaction data into the database
+        """
+
+        table = f"internal_transaction_data_{self.node_name}"
+       
+        await self.db.execute(f"""
+            INSERT INTO {table} (transaction_hash, block_number, from_address,to_address, value, transaction_fee, timestamp, miner, parent_hash, omner_hash, block_reward, uncles_reward)
+            VALUES ($1, $2, $3,$4, $5, $6,$7,$8, $9, $10, $11, $12);
         """, hash, difficulty, gas_limit)
+
+
+    async def insert_transaction_log_data(self, unique_id: int, transaction_hash: str, address: str, log_index: int, data: str, block_hash: str, block_number: int, removed: bool, topics: list[str]):
+        """
+        Insert transaction data into the database
+        """
+
+        table = f"transaction_log_data_{self.node_name}"
+       
+        await self.db.execute(f"""
+            INSERT INTO {table} (unique_id, transaction_hash, address, log_index, data, black_hash, block_number, removed, topics)
+            VALUES ($1, $2, $3,$4, $5, $6,$7,$8, $9);
+        """, self.unique_id, self.transaction_hash, self.address, self.log_index, self.data, self.black_hash, self.block_number, self.removed, self.topics)
+
+    
--- src/db/sql/00-init.sql
+++ src/db/sql/00-init.sql
@@ -1,23 +1,24 @@
 -- https://dba.stackexchange.com/a/42930
 
-CREATE OR REPLACE FUNCTION create_table_block_data(node_name varchar(3))
+CREATE OR REPLACE FUNCTION create_table_block(node_name varchar(3))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
 BEGIN
    EXECUTE format('
       CREATE TABLE IF NOT EXISTS %I (
-       number_serial PRIMARY KEY NOT NULL,
+       block_number bigint PRIMARY KEY NOT NULL,
        block_hash varchar(256) NOT NULL,
+       nonce varchar(256) NOT NULL,
        difficulty numeric(78,0) NOT NULL,
        gas_limit bigint NOT NULL,
        gas_used bigint NOT NULL,
-       timestamp timestamp NOT NULL,
+       timestamp varchar(256) NOT NULL,
        miner varchar(256) NOT NULL,
        parent_hash varchar(256) NOT NULL,
        omner_hash varchar(256) NOT NULL,
        block_reward numeric(78,18) NOT NULL,
-       uncles_reward, numeric(78,18) NOT NULL
+       uncles_reward numeric(78,18) NOT NULL
       )', 'block_data_' || node_name);
 END
 $func$;
@@ -27,26 +28,25 @@ SELECT create_table_block_data('etc');
 SELECT create_table_block_data('bsc');
 
 
-CREATE OR REPLACE FUNCTION create_table_transaction_data(node_name varchar(3))
+CREATE OR REPLACE FUNCTION create_table_transaction(node_name varchar(3))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
 BEGIN
    EXECUTE format('
       CREATE TABLE IF NOT EXISTS %I (
-       transaction_hash varchar(256),
-       block_number REFERENCES %I bigint NOT NULL,
-       timestamp timestamp NOT NULL,
+       transaction_hash varchar(256) PRIMARY KEY NOT NULL,
+       block_number bigint REFERENCES %I NOT NULL,
        from_address varchar(256) NOT NULL,
-       to_address varchar(256) NOT NULl
-       value numeric(78,0) NOT NULL,
+       to_address varchar(256) NOT NULl,
+       value numeric(78,18),
        transaction_fee numeric(78,18) NOT NULL,
        gas_price numeric(78,18) NOT NULL,
        gas_limit numeric(78,0) NOT NULL,
        gas_used numeric(78,0) NOT NULL,
-       input_data, varchar(128) NOT NULL
+       is_token_tx boolean NOT NULL,
+       input_data varchar(256) NOT NULL
       )', 'transaction_data_' || node_name, 'block_data_' || node_name);
-      -- is the input_data only 128 chars long?
 END
 $func$;
 
@@ -55,7 +55,7 @@ SELECT create_table_transaction_data('etc');
 SELECT create_table_transaction_data('bsc');
 
 
-CREATE OR REPLACE FUNCTION create_table_internal_transaction_data(node_name varchar(3))
+CREATE OR REPLACE FUNCTION create_table_internal_transaction(node_name varchar(3))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
@@ -63,19 +63,19 @@ BEGIN
    EXECUTE format('
       CREATE TABLE IF NOT EXISTS %I (
        unique_id PRIMARY KEY NOT NULL,
-       block_hash varchar(256) NOT NULL
-       transaction_hash varchar(256) NOT NULL ,
+       transaction_hash varchar(256) REFERENCES %I NOT NULL,
+       block_hash varchar(256) NOT NULL,
        block_number bigint,
-       timestamp timestamp NOT NULL ,
+       timestamp varchar(256) NOT NULL,
        from_address varchar(256) NOT NULL ,
-       to_address varchar(256) NOT NULL
-       value bigint,
+       to_address varchar(256) NOT NULL,
+       value numeric(78,18),
        gas_price bigint,
        gas_limit bigint NOT NULL,
        gas_used bigint NOT NULL,
-       input_data varchar(128) NOT NULL,
-       function_type,
-      )', 'internal_transaction_data_' || node_name);
+       input_data varchar(256) NOT NULL,
+       function_type varchar(256) NOT NULL,
+      )', 'internal_transaction_data_' || node_name,'transaction_data_' || node_name );
 END
 $func$;
 
@@ -85,7 +85,7 @@ SELECT create_table_internal_transaction_data('bsc');
 
 
 
-CREATE OR REPLACE FUNCTION create_table_transaction_logs_data(node_name varchar(3))
+CREATE OR REPLACE FUNCTION create_table_transaction_logs(node_name varchar(3))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
@@ -93,15 +93,15 @@ BEGIN
    EXECUTE format('
       CREATE TABLE IF NOT EXISTS %I (
        unique_id PRIMARY KEY,
-       transaction_hash varchar(256) NOT NULL,
+       transaction_hash varchar(256) REFERENCES %I NOT NULL,
+       address varchar(256) NOT NULL,
+       log_index int, 
+       data varchar(256) NOT NULL
        block_hash varchar(256) NOT NULL,
        block_number bigint,
-       contract_Address varchar(256),
-       from varchar(256) NOT NULL,
-       to varchar(256) NOT NULL,
-       gas_used bigint NOT NULL,
-       logs_bloom varchar(256) NOT NULL,
-      )', 'transaction_log_data_' || node_name);
+       removed boolean NOT NULL,
+       topics varchar(256) ARRAY[]
+      )', 'transaction_log_data_' || node_name, 'transaction_data_' || node_name);
 END
 $func$;
 
