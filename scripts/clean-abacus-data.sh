9c659ac0ddfe97dae0ba6e025138756589504465
dvdblk <david.bielik@uzh.ch>
2023-03-26 15:44:41 +0000

    Remove clean-abacus-data.sh

--- scripts/clean-abacus-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DATA_DIR=./data
-
-docker run --rm -v ${DATA_DIR}:/tmprm ubuntu rm -r /tmprm/kafka-data
-docker run --rm -v ${DATA_DIR}:/tmprm ubuntu rm -r /tmprm/zookeeper-data
-rm -r ${DATA_DIR}
