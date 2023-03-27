c159f5d8e0e8bc8432a05d6e5f85a189bb6c5042
dvdblk <david.bielik@uzh.ch>
2023-03-27 00:15:18 +0000

    Fix run scripts exit code on cleanup

--- scripts/util/compose-cleanup.sh
+++ scripts/util/compose-cleanup.sh
@@ -2,8 +2,10 @@
 
 # Cleanup on exit or interrupt
 function cleanup {
+    exit_status=$?
     echo "Starting cleanup; removing containers..."
     docker compose -p $PROJECT_NAME down --remove-orphans
     echo "Cleanup successful."
+    exit $exit_status
 }
 trap cleanup EXIT
