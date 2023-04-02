fce96720fa48583c14a881805b3afe84f3ce103e
hilalcomak <hilal.comak@uzh.ch>
2023-04-02 23:23:44 +0200

    Add logs when database exception

--- src/data_collection/app/db/manager.py
+++ src/data_collection/app/db/manager.py
@@ -166,20 +166,29 @@ class DatabaseManager:
         """
 
         table = f"{self.node_name}_transaction_logs"
-
-        await self.db.execute(
-            f"""
-            INSERT INTO {table} (transaction_hash, address, log_index, data, removed, topics)
-            VALUES ($1, $2, $3,$4, $5, $6)
-            ON CONFLICT (unique_id) DO NOTHING;
-            """,
-            transaction_hash,
-            address,
-            log_index,
-            data,
-            removed,
-            topics,
-        )
+        try:
+            await self.db.execute(
+                f"""
+                INSERT INTO {table} (transaction_hash, address, log_index, data, removed, topics)
+                VALUES ($1, $2, $3,$4, $5, $6)
+                ON CONFLICT (unique_id) DO NOTHING;
+                """,
+                transaction_hash,
+                address,
+                log_index,
+                data,
+                removed,
+                topics,
+                )
+        except asyncpg.exceptions.StringDataRightTruncationError as e:
+            log.error("transaction_hash: " + transaction_hash + \
+                      "address: " + address + \
+                      "log_index: " + log_index + \
+                      "data: " + data + \
+                      "removed: " + removed + \
+                      "topics: " + topics
+                      )
+            raise e
 
     async def insert_contract(
         self, address: str, transaction_hash: str, is_pair_contract: bool
