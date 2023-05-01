d122acfe62817c84572c1c2a68499d7c6da255c6
dvdblk <david.bielik@uzh.ch>
2023-05-01 18:58:03 +0000

    Update scripts/ dir
    * Update docker compose files, dev and prod only differ in the command
      configuration
    * Update scripts/README.md
    * Add scripts/stop-prod-eth.sh to stop running prod containers
    * Add scripts/view-logs-prod-eth.sh to view running prod container logs
    * Update run-dev script to connect container to bridge
    * Update run-prod script to not have a cleanup function

--- scripts/stop-prod-eth.sh
+++ scripts/stop-prod-eth.sh
@@ -3,4 +3,15 @@
 set -e
 source scripts/util/prepare-env.sh
 
-docker compose -p $PROJECT_NAME down --remove-orphans
+function cleanup {
+    echo "Exiting production containters; starting cleanup..."
+    docker compose -p $PROJECT_NAME down --remove-orphans
+    echo "Cleanup successful."
+}
+
+read -p "Are you sure you want to stop the 'prod' containers (y/n)? " choice
+case "$choice" in
+  y|Y ) cleanup;;
+  n|N ) echo "Ok - skipping...";;
+  * ) echo "Invalid value.";;
+esac
