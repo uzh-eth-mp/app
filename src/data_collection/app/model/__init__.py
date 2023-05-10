e20830dcaa9c0b2145d2676002acc8471bdb96c2
dvdblk <david.bielik@uzh.ch>
2023-05-10 08:28:45 +0000

    refactor: cfg.producer_type to mode

--- src/data_collection/app/model/__init__.py
+++ src/data_collection/app/model/__init__.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import auto, Enum, StrEnum
 from typing import List
 
 from hexbytes import HexBytes
@@ -10,6 +10,32 @@ class DataCollectionWorkerType(Enum):
 
     PRODUCER = "producer"
     CONSUMER = "consumer"
+
+
+class DataCollectionMode(StrEnum):
+    """The data collection mode."""
+
+    FULL = auto()
+    """FULL data collection mode, ignores "contracts" field in the config file
+
+    Note:
+        producer will go through every block and produce every transaction in this block to a Kafka topic
+        consumer will insert every received transaction to the database
+    """
+    PARTIAL = auto()
+    """PARTIAL data collection mode, respects "contracts" field in the config file
+
+    Note:
+        producer will go through every block and produce every transaction in this block to a Kafka topic
+        consumer will insert only transactions that are related to the contracts in the config file
+    """
+    LOG_FILTER = auto()
+    """LOG_FILTER data collection mode
+
+    Note:
+        producer will respect log_filter topics while producing transaction hashes to a Kafka topic
+        consumers will insert every received transaction to the database
+    """
 
 
 class Web3BaseModel(BaseModel):
