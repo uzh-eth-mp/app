f300b31e24ca6e4cecf2b68207dd8329e82f7c18
dvdblk <david.bielik@uzh.ch>
2023-05-12 01:42:23 +0000

    test(data_collector): context manager

--- src/data_collection/tests/unit/utils/test_data_collector.py
+++ src/data_collection/tests/unit/utils/test_data_collector.py
@@ -38,3 +38,33 @@ class TestKafkaEvent:
         ).decode_kafka_event(event)
         assert decoded_mode == DataCollectionMode(collection_mode)
         assert decoded_tx_hash == tx_hash
+
+
+class TestContextManager:
+    """Test context manager for data collector"""
+
+    async def test_aenter(self, default_config):
+        """Test __aenter__ method calls connect on kafka and db manager"""
+        data_collector = DataCollector(config=default_config)
+        data_collector.kafka_manager = AsyncMock()
+        data_collector.db_manager = AsyncMock()
+
+        data_collector.kafka_manager.connect.assert_not_awaited()
+        data_collector.db_manager.connect.assert_not_awaited()
+
+        await data_collector.__aenter__()
+
+        data_collector.kafka_manager.connect.assert_awaited_once()
+        data_collector.db_manager.connect.assert_awaited_once()
+
+    async def test_aexit(self, default_config):
+        """Test __aexit__ method calls disconnect on kafka manager"""
+        data_collector = DataCollector(config=default_config)
+        data_collector.kafka_manager = AsyncMock()
+        data_collector.db_manager = AsyncMock()
+
+        data_collector.kafka_manager.disconnect.assert_not_awaited()
+
+        await data_collector.__aexit__(None, None, None)
+
+        data_collector.kafka_manager.disconnect.assert_awaited_once()
