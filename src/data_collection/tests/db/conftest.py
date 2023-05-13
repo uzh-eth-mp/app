61d91e86b6217590b3087ef794fd366d6b1abf76
Can Inan <cinan@abacus-3.ifi.uzh.ch>
2023-05-13 19:28:22 +0000

    fixed tests db bytes

--- src/data_collection/tests/db/conftest.py
+++ src/data_collection/tests/db/conftest.py
@@ -75,26 +75,26 @@ async def clean_redis(redis_manager):
     await redis_manager.redis.flushall()
 
 
-shared_tx_hash = "0xa76bef720a7093e99ce5532988623aaf62b490ecba52d1a94cb6e118ccb56822"
-shared_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+shared_tx_hash = b"0xa76bef720a7093e99ce5532988623aaf62b490ecba52d1a94cb6e118ccb56822"
+shared_address = b"0xdAC17F958D2ee523a2206206994597C13D831ec7"
 
 
 @pytest.fixture
 def block_data() -> dict[str, Any]:
     return dict(
         block_number=10,
-        block_hash="0x98bdfb36d9d0d259afa8403b62255e0c04161a341a2aa646dbeae6d1c32dcb9d",
-        nonce="0x8aee4c3380578665",
+        block_hash= b"0x98bdfb36d9d0d259afa8403b62255e0c04161a341a2aa646dbeae6d1c32dcb9d",
+        nonce= b"0x8aee4c3380578665",
         difficulty=76543786543216789,
         gas_limit=30029295,
         gas_used=30025828,
         timestamp=datetime.fromtimestamp(1661048118),
-        miner="0xea674fdde714fd979de3edf0f56aa9716b898ec8",
-        parent_hash="0x3d1131b702e9aa071fb15970b4271cde104e5802b542f478782f5b1ef0ffa436",
+        miner= b"0xea674fdde714fd979de3edf0f56aa9716b898ec8",
+        parent_hash= b"0x3d1131b702e9aa071fb15970b4271cde104e5802b542f478782f5b1ef0ffa436",
         static_block_reward=2.052229489633251327,
-        uncles = ["0x3d1131b702e9aa071fb15970b4271cde104e5802b542f478782f5b1ef0ffa436",
-                "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-                "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d"]
+        uncles = [b"0x3d1131b702e9aa071fb15970b4271cde104e5802b542f478782f5b1ef0ffa436",
+                b"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                b"0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d"]
     )
 
 
@@ -103,15 +103,15 @@ def transaction_data() -> dict[str, Any]:
     return dict(
         transaction_hash=shared_tx_hash,
         block_number=10,
-        from_address="0xca44f331c32783cc6678cb5ffaa2b6739299b42b",
-        to_address="0xd26a2ba4472a50ea23bef8189a3de4911af03dad",
+        from_address= b"0xca44f331c32783cc6678cb5ffaa2b6739299b42b",
+        to_address= b"0xd26a2ba4472a50ea23bef8189a3de4911af03dad",
         value=0.000000487343799995,
         transaction_fee=0.000048247036173,
         gas_price=0.000000002297477913,
         gas_limit=21000,
         gas_used=21000,
         is_token_tx=False,
-        input_data="0x",
+        input_data= b"0x",
     )
 
 
@@ -119,13 +119,13 @@ def transaction_data() -> dict[str, Any]:
 def internal_transaction_data() -> dict[str, Any]:
     return dict(
         transaction_hash=shared_tx_hash,
-        from_address="0xca44f331c32783cc6678cb5ffaa2b6739299b42b",
-        to_address="0xd26a2ba4472a50ea23bef8189a3de4911af03dad",
+        from_address= b"0xca44f331c32783cc6678cb5ffaa2b6739299b42b",
+        to_address= b"0xd26a2ba4472a50ea23bef8189a3de4911af03dad",
         value=0.000000487343799995,
         gas_limit=21000,
         gas_used=21000,
-        input_data="xd",
-        call_type="CREATE",
+        input_data= b"xd",
+        call_type= b"CREATE",
     )
 
 
@@ -133,15 +133,15 @@ def internal_transaction_data() -> dict[str, Any]:
 def transaction_logs_data() -> dict[str, Any]:
     return dict(
         transaction_hash=shared_tx_hash,
-        address="0xf76de79a8cb78158f22dc8e0f3b6f3f6b9cd97d8",
+        address= b"0xf76de79a8cb78158f22dc8e0f3b6f3f6b9cd97d8",
         log_index=0,
-        data="|Â¦E<",
+        data= bytes("|Â¦E<","utf-8"),
         removed=False,
         topics=[
-            "0x940c4b3549ef0aaff95807dc27f62d88ca15532d1bf535d7d63800f40395d16c",
-            "0x000000000000000000000000e2de6d17b8314f7f182f698606a53a064b00ddcc",
-            "0x0000000000000000000000005e42c86bb5352e9d985dd1200e05a35f4b0b2b14",
-            "0x54494d4500000000000000000000000000000000000000000000000000000000",
+            b"0x940c4b3549ef0aaff95807dc27f62d88ca15532d1bf535d7d63800f40395d16c",
+            b"0x000000000000000000000000e2de6d17b8314f7f182f698606a53a064b00ddcc",
+            b"0x0000000000000000000000005e42c86bb5352e9d985dd1200e05a35f4b0b2b14",
+            b"0x54494d4500000000000000000000000000000000000000000000000000000000",
         ],
     )
 
@@ -150,7 +150,7 @@ def transaction_logs_data() -> dict[str, Any]:
 def contract_data() -> dict[str, Any]:
     return dict(
         address=shared_address,
-        transaction_hash="0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca77",
+        transaction_hash= b"0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca77",
         is_pair_contract=True,
     )
 
@@ -158,11 +158,11 @@ def contract_data() -> dict[str, Any]:
 @pytest.fixture
 def pair_contract_data() -> dict[str, Any]:
     return dict(
-        token0_address="0xCAFE000000000000000000000000000000000000",
-        token1_address="0xF00D000000000000000000000000000000000000",
+        token0_address= b"0xCAFE000000000000000000000000000000000000",
+        token1_address= b"0xF00D000000000000000000000000000000000000",
         reserve0=1000,
         reserve1=900,
-        factory="0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+        factory= b"0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
         address=shared_address,
     )
 
@@ -173,7 +173,7 @@ def pair_liquidity_change_data() -> dict[str, Any]:
         address=shared_address,
         amount0=100,
         amount1=90,
-        transaction_hash="0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca77",
+        transaction_hash= b"0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca77",
     )
 
 
@@ -181,11 +181,11 @@ def pair_liquidity_change_data() -> dict[str, Any]:
 def token_contract_data() -> dict[str, Any]:
     return dict(
         address=shared_address,
-        symbol="USDT",
-        name="Tether",
+        symbol= b"USDT",
+        name= b"Tether",
         decimals=6,
         total_supply=1000000,
-        token_category="erc20",
+        token_category= "erc20",
     )
 
 
@@ -194,7 +194,7 @@ def contract_supply_change_data() -> dict[str, Any]:
     return dict(
         address=shared_address,
         amount_changed=22,
-        transaction_hash="0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca78",
+        transaction_hash= b"0x871dadd3f5ca95b398575d710d39385de9123a10717e2e37a90545a805daca78",
     )
 
 
@@ -204,6 +204,6 @@ def nft_transfer_data() -> dict[str, Any]:
         transaction_hash=shared_tx_hash,
         address=shared_address,
         token_id=1,
-        from_address="0xCAFE000000000000000000000000000000000000",
-        to_address="0xF00D000000000000000000000000000000000000",
+        from_address= b"0xCAFE000000000000000000000000000000000000",
+        to_address= b"0xF00D000000000000000000000000000000000000",
     )
