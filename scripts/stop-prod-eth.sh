5505cf92d33f2db271441d8aaada6ef3bf5afcc1
David Bielik <git@dvdblk.com>
2023-05-21 17:22:27 +0200

    feat: stop-prod-eth does docker stop

--- scripts/stop-prod-eth.sh
+++ scripts/stop-prod-eth.sh
@@ -4,12 +4,12 @@ set -e
 source scripts/util/prepare-env.sh
 
 function cleanup {
-    echo "Exiting production containters; starting cleanup..."
-    docker compose -p $PROJECT_NAME down --remove-orphans
-    echo "Cleanup successful."
+    echo "Stopping production containters;"
+    docker compose -p $PROJECT_NAME stop
+    echo "Success."
 }
 
-read -p "Are you sure you want to stop the 'prod' containers (y/n)? " choice
+read -p "Are you sure you want to stop the 'prod' containers? This will not remove them. (y/n) " choice
 case "$choice" in
   y|Y ) cleanup;;
   n|N ) echo "Ok - skipping...";;
